4f21ee442904b2c31a6840fc960dae2ce5806c9b
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-02-03 13:24:50 -0500

    Update threesaveslot.py

--- threesaveslot/threesaveslot.py
+++ threesaveslot/threesaveslot.py
@@ -3,3 +3,13 @@ moduleName.py
 
 This is where all the functions, classes, variables, etc lie in your custom module.
 """
+
+savelist = []
+
+
+def createsaveslot(name = "savefile"):
+  open(name, "x")
+
+
+def basicsave(listtosave = savelist, filename = "savefile"):
+  filename = str(filename)
