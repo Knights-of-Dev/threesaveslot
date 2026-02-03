66abee4efee19a69ece750e1ccc343b3da293a07
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-02-03 13:38:14 -0500

    Update threesaveslot.py

--- threesaveslot/threesaveslot.py
+++ threesaveslot/threesaveslot.py
@@ -3,13 +3,24 @@ moduleName.py
 
 This is where all the functions, classes, variables, etc lie in your custom module.
 """
+import json
+
 
 savelist = []
 
 
-def createsaveslot(name = "savefile"):
+def createsavefile(name = "savefile"):
   open(name, "x")
 
 
 def basicsave(listtosave = savelist, filename = "savefile"):
   filename = str(filename)
+  with open(str(filename + ".json"), "w") as f:
+    json.dump(listtosave, f)
+
+
+def basicload(filename = "savefile"):
+  filename = str(filename)
+  open(filename + ".json") as f:
+    return json.load(f)
+  
